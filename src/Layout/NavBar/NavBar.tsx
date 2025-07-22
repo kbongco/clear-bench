ef206e98a0c1e96966a26ef12cdef17540f95ebf
Kathleen B <kathleen.bongco@gmail.com>
2025-07-22 14:08:56 -0400

    added in font awesome and added in icons

--- src/Layout/NavBar/NavBar.tsx
+++ src/Layout/NavBar/NavBar.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faHouse, faFlask, faCheckSquare, faFile, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 export default function NavBar() {
   return (
     <>
@@ -6,30 +8,71 @@ export default function NavBar() {
           <h1 className='text-2x text-white font-bold'>Sample Management</h1>
         </div>
         <hr />
-        <div className='flex'>
-          <ul className='m-8 flex flex-col'>
+        <div className='flex '>
+          <ul className='m-4 flex flex-col'>
             <li className='m-8'>
-              <a href='/' className='text-white text-2x font-bold'>Home</a>
+              <div className='flex'>
+                <div className='gap-4'>
+                  <FontAwesomeIcon className='text-2xl text-white' icon={faHouse} />
+                </div>
+                <div>
+                  <a href='/' className='text-white text-2x font-bold'>Home</a>
+                </div>
+              </div>
             </li>
             <li className='m-8'>
-              <a href='/notifications' className='text-white text-2x font-bold'>Notifications</a>
+              <div className='flex'>
+                <div className='gap-4'>
+                  <FontAwesomeIcon className='text-2xl text-white' icon={faBell} />
+                </div>
+                <a href='/notifications' className='text-white text-2x font-bold'>Notifications</a>
+              </div>
             </li>
             <li className='m-8'>
-              <a href='/all-samples' className='text-white text-2x font-bold'>All Samples</a>
+              <div className='flex'>
+                <div className='gap-4'>
+                  <FontAwesomeIcon className='text-2xl text-white' icon={faFlask} />
+                </div>
+                <div>
+                  <a href='/all-samples' className='text-white text-2x font-bold'>All Samples</a>
+                </div>
+              </div>
             </li>
             <li className='m-8'>
-              <a href='/current-samples' className='text-white text-2x font-bold'>Current Samples</a>
+              <div className='flex'>
+                <div className='gap-4'>
+                  <FontAwesomeIcon className='text-2xl text-white' icon={faFlask} />
+                </div>
+                <div>
+                  <a href='/current-samples' className='text-white text-2x font-bold'>Current Samples</a>
+                </div>
+              </div>
             </li>
             <li className='m-8'>
-              <a href='/submit-samples' className='text-white text-2x font-bold'>Submit Samples</a>
+              <div className='flex'>
+                <div className='gap-4'>
+                  <FontAwesomeIcon className='text-2xl text-white' icon={faCheckSquare} />
+                </div>
+                <div>
+                  <a href='/submit-samples' className='text-white text-2x font-bold'>Submit Samples</a>
+                </div>
+              </div>
             </li>
             <li className='m-8'>
-              <a href='/generate-report' className='text-white text-2x font-bold'>Generate Report</a>
+            <div className='flex'>
+                <div className='gap-4'>
+                <FontAwesomeIcon className='text-2xl text-white' icon={faFile} />
+                </div>
+                <div>
+                <a href='/generate-report' className='text-white text-2x font-bold'>Generate Report</a> 
+                </div>
+              </div>
             </li>
           </ul>
         </div>
         <hr />
-        <div>
+        <div className='flex'>
+        <FontAwesomeIcon className='text-2xl text-white' icon={faRightFromBracket} />
           <h1 className='text-2x text-white font-bold'>Logout</h1>
         </div>
       </div>
