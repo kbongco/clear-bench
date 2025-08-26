f90bbfe97a876746c8233a2aa88bf178fa992de3
Kathleen B <kathleen.bongco@gmail.com>
2025-08-26 12:33:17 -0400

    added in data on top

--- src/Views/SampleView.tsx
+++ src/Views/SampleView.tsx
@@ -4,16 +4,41 @@ export default function SampleView({ data }: any) {
   // console.log(data[0].sample.name, 'data')
   console.log(data[0]?.sample?.out_of_spec, 'datas');
   const sample = data?.[0]?.sample;
-    const outOfSpecStats = sample ? (sample.out_of_spec ? "True" : "False") : "N/A"
-  console.log(data, 'datas');
+
+  if (!sample) {
+    // Optionally show a loading state
+    return <p>Loading...</p>;
+  }
+
+  const scientistName = sample.scientist?.name || "N/A";
+  const labTechName = sample.lab_tech?.name || "N/A";
+  const outOfSpecStats = sample.out_of_spec ? "True" : "False";
   return ( 
     <>
-      <div className='p-10 flex justify-between'>
+    <div className='pt-10 px-10 pb-5'>
+      <div className="grid grid-cols-3 gap-4 items-center">
         <h1 className='text-5xl'>{data[0]?.sample?.name}</h1>
-        <div className='flex content-center items-center gap-3'>
+        <div></div> 
+        <div className='flex content-center items-center gap-3 justify-end'>
           <SpecIndicator stats={outOfSpecStats} />
+        </div>
+      </div>
+    </div>
+  
+      <div className="px-10">
+        <div className="grid grid-cols-3 gap-4 text-3xl">
+          <h2>{data[0]?.sample?.id}</h2>
+          <h2>{data[0]?.sample?.test_start}</h2>
+          <h2>{data[0]?.sample?.due_date}</h2>
+        </div>
+  
+        <div className="grid grid-cols-3 gap-4 text-2xl pt-5">
+          <h2>{scientistName}</h2>
+          <h2>{labTechName}</h2>
+          <div></div> {/* empty div to maintain grid structure */}
         </div>
       </div>
     </>
   )
+  
 }
