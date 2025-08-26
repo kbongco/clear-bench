b082b898593bce580c925186ac563704f08ea702
Kathleen B <kathleen.bongco@gmail.com>
2025-08-26 12:03:09 -0400

    completed spec indicator component

--- src/Components/SpecIndicator/SpecIndicator.tsx
+++ src/Components/SpecIndicator/SpecIndicator.tsx
@@ -1,13 +1,19 @@
-interface SpecIndicator {
-  word: string;
-  status: string;
+interface SpecIndicatorProps {
+  stats: string;
 }
 
-export default function SpecIndicator() {
+export default function SpecIndicator({ stats }: SpecIndicatorProps) {
   return (
     <>
-      <div className='h-5 w-5 bg-green-800 bg-green-800 rounded-lg '/>
-      <h2 className='text-3xl'>Spec Indicator</h2>
+      <div className={`h-5 w-5 rounded-lg ${stats === 'False' ? 'bg-green-800' : 'bg-red-600'}`} />
+      <div>
+        <div>
+          <h2 className='text-3xl'>{stats === 'False' ? 'Currently In Spec' : "Out of Spec"}</h2>
+          {stats !== 'False' && (
+            <p className="text-red-600 font-bold mt-2">Needs attention!</p>
+          )}
+        </div>
+      </div>
     </>
   )
 }
--- src/Views/SampleView.tsx
+++ src/Views/SampleView.tsx
@@ -4,14 +4,14 @@ export default function SampleView({ data }: any) {
   // console.log(data[0].sample.name, 'data')
   console.log(data[0]?.sample?.out_of_spec, 'datas');
   const sample = data?.[0]?.sample;
+    const outOfSpecStats = sample ? (sample.out_of_spec ? "True" : "False") : "N/A"
   console.log(data, 'datas');
   return ( 
     <>
       <div className='p-10 flex justify-between'>
         <h1 className='text-5xl'>{data[0]?.sample?.name}</h1>
         <div className='flex content-center items-center gap-3'>
-          <SpecIndicator/>
-          {/* <h2 className='text-3xl'>{sample ? (sample.out_of_spec ? "True" : "False") : "N/A"}</h2> */}
+          <SpecIndicator stats={outOfSpecStats} />
         </div>
       </div>
     </>
