1238ecf720d938588c6bb1ee54dd533f6d3f3b52
Kathleen B <kathleen.bongco@gmail.com>
2025-08-26 11:32:49 -0400

    starting sample page UI

--- src/Views/SampleView.tsx
+++ src/Views/SampleView.tsx
@@ -1,8 +1,16 @@
 export default function SampleView({ data }: any) {
-  console.log(data,'data')
+  // console.log(data[0].sample.name, 'data')
+  console.log(data[0]?.sample?.out_of_spec, 'datas');
+  const sample = data?.[0]?.sample;
+  console.log(data, 'datas');
   return ( 
     <>
-      <h1>Sample data view</h1>
+      <div className='p-10 flex justify-between'>
+        <h1 className='text-5xl'>{data[0]?.sample?.name}</h1>
+        <div className='flex content-center items-center'>
+          <h2 className='text-3xl'>{sample ? (sample.out_of_spec ? "True" : "False") : "N/A"}</h2>
+        </div>
+      </div>
     </>
   )
 }
