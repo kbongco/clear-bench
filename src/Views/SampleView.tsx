fb8723bd3666a0c1f151256adca15abf4f95a705
Kathleen B <kathleen.bongco@gmail.com>
2025-08-29 10:18:36 -0400

    transformed data, removed mock data and using data from API to fill in the gaps now

--- src/Views/SampleView.tsx
+++ src/Views/SampleView.tsx
@@ -1,14 +1,12 @@
+import { useMemo } from "react";
 import SpecIndicator from "../Components/SpecIndicator/SpecIndicator";
 import Table from "../Components/Table/Table";
 import TabLayout from "../Components/Tabs/TabLayout";
 import Tabs from "../Components/Tabs/Tabs";
 
 export default function SampleView({ data }: any) {
-  console.log(data[0]?.sample?.out_of_spec, 'datas');
   const sample = data?.[0]?.sample;
   const results = data?.[0]?.results;
-  console.log(data?.[0]?.sample, 'sample');
-  console.log(data?.[0]?.results);
 
   if (!sample) {
     return <p>Loading...</p>;
@@ -21,38 +19,18 @@ export default function SampleView({ data }: any) {
   const scientistName = sample.scientist?.name || "N/A";
   const labTechName = sample.lab_tech?.name || "N/A";
   const outOfSpecStats = sample.out_of_spec ? "True" : "False";
-  const resultsTable = 'Sample Results'
-  const mockData = [
-    {
-      expected_range_max: 7.5,
-      expected_range_min: 6.5,
-      id: 1,
-      is_within_spec: false,
-      measured_value: 6.2,
-      notes: "Low pH detected.",
-      parameter_name: "pH",
-      result_id: 1,
-      specification_limit: 6.5,
-      unit: ""
-    },
-    {
-      expected_range_max: 110,
-      expected_range_min: 100,
-      id: 2,
-      is_within_spec: false,
-      measured_value: 120.5,
-      notes: "Exceeds upper limit.",
-      parameter_name: "Concentration",
-      result_id: 1,
-      specification_limit: 110,
-      unit: "mg/L"
-    }
-  ];
-  console.log(data[0]?.results, 'res')
-  const minPh = data?.[0]?.results.expected_range_min;
-  const maxPh = results.expected_range_max;
-  console.log(minPh, maxPh, 'minMax');
-  console.log(data);
+  const resultsTable = 'Sample Results';
+
+  const transformedResults = useMemo(() => {
+    return results.map((res) => ({
+      id: res.id,
+      TestName: res.parameter_name,
+      TestValue: res.measured_value,
+      TestUnit: res.unit,
+      MinRange: res.expected_range_min,
+      MaxRange: res.expected_range_max
+    }))
+  }, [results]);
   return (
     <>
       <div className='pt-10 px-10 pb-5'>
@@ -82,6 +60,7 @@ export default function SampleView({ data }: any) {
       <div className='p-10'>
         <Tabs>
           <TabLayout title="Overview">
+            {/* Clean this up */}
             <div className='py-10'>
               <h1 className='text-3xl'>{data[0]?.sample?.name}</h1>
               <h2 className='text-3xl py-2'>{data[0]?.sample?.test_start} - {data[0]?.sample?.due_date}</h2>
@@ -92,15 +71,19 @@ export default function SampleView({ data }: any) {
             </div>
             <div className='py-4'>
               <h3 className='text-3xl'>Sample Specifications:</h3>
-              <h4 className='text-3xl'> Min pH:{data[0]?.results?.expected_range_min} </h4>
-              <h4 className='text-3xl'> Max pH:{data[0]?.results?.expected_range_min} </h4>
+              <h4 className='text-3xl'> Min pH:{transformedResults[0]?.MinRange} </h4>
+              <h4 className='text-3xl'> Max pH:{transformedResults[0]?.MaxRange} </h4>
+              <div className='pt-4'>
+                <h4 className='text-3xl'>Concentration:{transformedResults[1]?.MinRange} {transformedResults[1]?.TestUnit
+                }</h4>
+              </div>
             </div>
           </TabLayout>
           <TabLayout title="Results">
             <div className='py-5'>
             </div>
                   <table className="w-full table-auto bg-white border-collapse border border-gray-200">
-              <Table tableTitle={resultsTable} tableHeader={['ID', 'Name', 'Value', 'Unit', 'Min Range', 'Max Range']} data={mockData} />
+              <Table tableTitle={resultsTable} tableHeader={['id', 'TestName', 'TestValue', 'TestUnit', 'MinRange', 'MaxRange']} data={transformedResults} />
               </table>
           </TabLayout>
           <TabLayout title="Graphs"><div className='py-10'>This is a test of the Content that is here</div></TabLayout>
